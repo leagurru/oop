a3ac4a3402ab1df829946cd2f16081de4d102603
Leandro Gurruchaga <leagurru@gmail.com>
2018-09-04 22:57:33 -0300

    first commit

---
+++ class_instance.php
@@ -0,0 +1,19 @@
+<?php
+
+class Cars {
+
+    public function greeting(){
+
+    }
+
+    public function greeting2(){
+
+    }
+}
+
+$the_methods = get_class_methods('Cars');
+foreach ($the_methods as $method) {
+    echo $method . "<br>";
+}
+
+    ?>
---
+++ class_properties.php
@@ -0,0 +1,14 @@
+<?php
+
+class Cars {
+
+    public function greeting(){
+        echo "Hola";
+    }
+
+}
+
+$bmw = new Cars();
+$bmw->greeting();
+
+?>
